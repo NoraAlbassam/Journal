2a7f0798387c1a721c1b18e93bd229f9a3da3027
nora <noralbassam@gmail.com>
2024-10-20 14:14:37 +0300

    Initial Commit

---
+++ Journal/JournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournalApp.swift
+//  Journal
+//
+//  Created by Nora on 20/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct JournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JournalTests/JournalTests.swift
@@ -0,0 +1,17 @@
+//
+//  JournalTests.swift
+//  JournalTests
+//
+//  Created by Nora on 20/10/2024.
+//
+
+import Testing
+@testable import Journal
+
+struct JournalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
